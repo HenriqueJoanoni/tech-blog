3e650a5589285f1efc786200765bb08d4c8a3311
Jose Henrique Joanoni <henrique.joanoni@gmail.com>
2025-02-20 20:46:49 +0000

    build(admin): Create logout route

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/delete-post/{id}', [AdminController::class, 'deletePost'])->name('admin.delete-post');
 
     Route::post('/posts/{id}/toggle-visibility', [AdminController::class, 'toggleVisibility'])->name('admin.toggle-visibility');
+
+    Route::get('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
 });
