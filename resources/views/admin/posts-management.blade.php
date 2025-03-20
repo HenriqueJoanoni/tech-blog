3b099dacaf8216f50d3625d332850fe46f8cb201
Jose Henrique Joanoni <henrique.joanoni@gmail.com>
2025-03-20 22:43:26 +0000

    refactor(admin): Fix display posts view bug

--- resources/views/admin/posts-management.blade.php
+++ resources/views/admin/posts-management.blade.php
@@ -11,59 +11,73 @@
                     <div class="card-header d-flex justify-content-between align-items-end position-relative py-4">
                         <h3 class="card-title">Welcome to Posts Management Page</h3>
                         <div class="insert-post-btn">
-                            <a class="btn btn-success" href="{{ route('admin.create-posts') }}">Create Post</a>
+                            <a class="btn btn-success" href="{{ route('admin.create-posts') }}">
+                                <i class="fas fa-plus-circle"></i> Create Post
+                            </a>
                         </div>
                     </div>
-                    <table class="table table-hover">
-                        <thead>
-                        <tr>
-                            <th scope="col">id</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Excerpt</th>
-                            <th scope="col">Views</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">Author</th>
-                            <th scope="col">Created at</th>
-                            <th scope="col">Updated at</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($posts as $post)
+                    @if($posts->count() > 0)
+                        <table class="table table-hover">
+                            <thead>
                             <tr>
-                                <th scope="row"><?= $post->id ?></th>
-                                <td><?= GeneralHandler::str_limit_words($post->title, 4) ?></td>
-                                <td><?= GeneralHandler::str_limit_words($post->excerpt, 4) ?></td>
-                                <td><?= $post->views ?? 0 ?></td>
-                                <td><?= $post->category->category_name ?></td>
-                                <td><?= $post->user->name ?></td>
-                                <td><?= GeneralHandler::dateFmt($post->created_at, 'd/m/Y') ?></td>
-                                <td><?= GeneralHandler::dateFmt($post->updated_at, 'd/m/Y') ?></td>
-                                <td>
-                                    <button type="button"
-                                            class="btn btn-warning"
-                                            onclick="updateVisibility(event, {{ $post->id }})">
-                                        @if($post->is_visible == 0)
-                                            <i class="fa fa-eye-slash"></i>
-                                        @else
-                                            <i class="fa fa-eye"></i>
-                                        @endif
-                                    </button>
-                                    <a href="{{ route('admin.edit-post', ['id' => $post->id]) }}"
-                                       class="btn btn-primary" title="Edit">
-                                        <i class="fa fa-pencil"></i>
-                                    </a>
-                                    <button class="btn btn-danger delete-post-btn" data-post-id="{{ $post->id }}" data-post-name="{{ $post->title }}">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                </td>
+                                <th scope="col">id</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Excerpt</th>
+                                <th scope="col">Views</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Author</th>
+                                <th scope="col">Created at</th>
+                                <th scope="col">Updated at</th>
+                                <th scope="col">Actions</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    <div class="align-self-center">
-                        {{ $posts->links('vendor.pagination.bootstrap-5') }}
-                    </div>
+                            </thead>
+                            <tbody>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <th scope="row">{{ $post->id }}</th>
+                                    <td>{{ GeneralHandler::str_limit_words($post->title, 4) }}</td>
+                                    <td>{{ GeneralHandler::str_limit_words($post->excerpt, 4) }}</td>
+                                    <td>{{ $post->views ?? 0 }}</td>
+                                    <td>{{ $post->category->category_name }}</td>
+                                    <td>{{ $post->user->name }}</td>
+                                    <td>{{ GeneralHandler::dateFmt($post->created_at, 'd/m/Y') }}</td>
+                                    <td>{{ GeneralHandler::dateFmt($post->updated_at, 'd/m/Y') }}</td>
+                                    <td>
+                                        <button type="button"
+                                                class="btn btn-warning"
+                                                onclick="updateVisibility(event, {{ $post->id }})">
+                                            @if($post->is_visible == 0)
+                                                <i class="fa fa-eye-slash"></i>
+                                            @else
+                                                <i class="fa fa-eye"></i>
+                                            @endif
+                                        </button>
+                                        <a href="{{ route('admin.edit-post', ['id' => $post->id]) }}"
+                                           class="btn btn-primary" title="Edit">
+                                            <i class="fa fa-pencil"></i>
+                                        </a>
+                                        <button class="btn btn-danger delete-post-btn" data-post-id="{{ $post->id }}" data-post-name="{{ $post->title }}">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        @if($posts->count() > 6)
+                                <div class="align-self-center">
+                                    {{ $posts->links('vendor.pagination.bootstrap-5') }}
+                                </div>
+                        @endif
+                    @else
+                        <div class="text-center py-5">
+                            <h4 class="text-muted mb-4">No posts found</h4>
+                            <p class="text-secondary">You haven't created any posts yet. Click the button below to create your first post!</p>
+                            <a class="btn btn-success" href="{{ route('admin.create-posts') }}">
+                                <i class="fas fa-plus-circle"></i> Create Post
+                            </a>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
